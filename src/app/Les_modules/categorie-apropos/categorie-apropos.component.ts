63d4d5447642c0a260066296c43b6154bc4247f6
Diakaridia SY <sydiakaridia38@outlook.com>
2023-04-19 22:02:57 +0000

    Ecommerce Original

---
+++ src/app/Les_modules/categorie-apropos/categorie-apropos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorie-apropos',
+  templateUrl: './categorie-apropos.component.html',
+  styleUrls: ['./categorie-apropos.component.css']
+})
+export class CategorieAPROPOSComponent {
+
+}
